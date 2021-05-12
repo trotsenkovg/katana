ab891cc0acd13505a68fa39cb07b54ea2cb0cb70
trovagen <trovagen@mirasvit.com>
2021-05-12 23:54:14 +0300

    Tailwindcss

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -3,9 +3,24 @@ import ReactDOM, {render} from "react-dom";
 
 const App = () => {
     return (
-            <div className="App">
-                <h1 align="center">Katana React</h1>
+        <div className="md:flex">
+            <div className="md:flex-shrink-0">
+                <img className="rounded-lg md:w-56" src="/img/shopping.jpg" alt="Woman paying for a purchase"/>
             </div>
+            <div className="mt-4 md:mt-0 md:ml-6">
+                <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+                    Marketing
+                </div>
+                <a href="/get-started"
+                   className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+                    Finding customers for your new business
+                </a>
+                <p className="mt-2 text-gray-600">
+                    Getting a new business off the ground is a lot of hard work.
+                    Here are five ideas you can use to find your first customers.
+                </p>
+            </div>
+        </div>
     )
 }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/assets/js')
     .react()
-    .sass('resources/assets/sass/app.scss', 'public/assets/css');
+    .postCss('resources/assets/sass/app.css', 'public/assets/css', [
+        require("tailwindcss")
+    ]);
